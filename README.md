52979aa0b37be7682f265b6c910b0a1e4e37d067
1BObC <bob.cocksedge@gmail.com>
2021-07-22 10:16:52 +0100

    Started setting up files for #168 to #171 Continue Friday 23July

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 "Restart project delete files except git ignore and README Install boilerplate Avoid npm audit problems by using npm install no audit - here's hoping!"
 "Complete #166 project preview & setup Setup ok so far!" 
 "Cannot complete #167 CSS & Style Loaders (webpack)   import './test.css' in index jscauses error Module build failed (from ./node_modules/postcss-loader/dist/cjs.js): TypeError: this.getOptions is not a function  ./src/index.js line 19 const options = this.getOptions(_options.default);  Will finish #168 to #172 inclusive then research the error!"
+"Started setting up files for #168 to #171 Continue Friday 23July"
--- src/index.js
+++ src/index.js
@@ -1,5 +1,39 @@
-import './test.css';
 
-console.log('index.js file');
-console.log('anybody there?');
+import './style.css';
+
+
+import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
+import Tabs from './ninja-ui/tabs';
+import Snackbar from './ninja-ui/snackbar';
+
+//#168 Tooltips
+// create tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+tooltip.init();
+
+//#169 Dropdowns
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
+
+//#170 Tabbed Content
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//#171 Snackbars
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  snackbar.show('you clicked me :)');
+});
+
 
